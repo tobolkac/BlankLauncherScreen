4131c3bd6f86b6a82efaeca67befeb5c7d733ff8
Clay Tobolka <tobolkac@gmail.com>
2015-10-10 13:38:17 -0500

    initial commit

---
+++ app/src/main/java/com/claytobolka/blanklauncherscreen/SimpleAppWidgetProvider.java
@@ -0,0 +1,9 @@
+package com.claytobolka.blanklauncherscreen;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by tobolkac on 10/10/15.
+ */
+public class SimpleAppWidgetProvider extends AppWidgetProvider {
+}
